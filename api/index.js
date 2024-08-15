a3599fe2df6cc4798f6f57850181507766e7df98
peace <peaceosenah@gmail.com>
2024-08-15 22:52:42 +0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 // Initialize http server
 const app = express();
 
+app.use(express.json()); // parse json bodies
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
@@ -27,3 +30,4 @@ app.listen(3000, () => {
 
 // Import routes
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
