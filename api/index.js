8aebcc032c2034b5ce0d695e3013104a490e4cb3
peace <peaceosenah@gmail.com>
2024-08-15 21:06:15 +0300

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log('Failed to connect to MongoDB', error);
+});
+
+// Initialize http server
 const app = express();
 
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+    console.log('Server is running on port 3000');
 });
