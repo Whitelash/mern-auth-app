7ae7221752a8c8fa6b607d20c0b5264a271ab5b7
peace <peaceosenah@gmail.com>
2024-08-16 15:11:29 +0300

    add middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -31,3 +31,14 @@ app.listen(3000, () => {
 // Import routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+//adding middleware
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+})
